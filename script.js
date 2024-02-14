e2205f310c9ae90ccfe9ec8156146149fa18948d
Ganesh-1907 <bora1132004@gmail.com>
2024-02-14 10:36:53 +0530

    Your descriptive commit message here

---
+++ script.js
@@ -0,0 +1,34 @@
+const apiKey = '4b79804b7b2878a9fdb84e816a7a6e07';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+const locationInput = document.getElementById('locationInput');
+const searchButton = document.getElementById('searchButton');
+const locationElement = document.getElementById('location');
+const temperatureElement = document.getElementById('temperature');
+const descriptionElement = document.getElementById('description');
+const humidityElement = document.getElementById('humidity');
+const windSpeedElement = document.getElementById('windSpeed');
+
+searchButton.addEventListener('click', () => {
+    const location = locationInput.value;
+    if (location) {
+        fetchWeather(location);
+    }
+});
+
+function fetchWeather(location) {
+    const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            locationElement.textContent = data.name;
+            temperatureElement.textContent = `temprature : ${Math.round(data.main.temp)}°C`;
+            descriptionElement.textContent = data.weather[0].description;
+            humidityElement.textContent = `Humidity: ${data.main.humidity}%`;
+            windSpeedElement.textContent = `Wind Speed: ${data.wind.speed} m/s`;
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+        });
+}
